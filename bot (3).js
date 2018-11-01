257243ce5b926a8334be19624c0762161bee0473
Riverr1 <44487673+Riverr1@users.noreply.github.com>
2018-11-01 14:36:01 +0400

    Update bot (3).js

--- bot (3).js
+++ bot (3).js
@@ -5,7 +5,7 @@ client.on('ready', () => {
      console.log(ON ${client.guilds.size} Servers '     Script By : EX Clan ');
    console.log(----------------);
  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(افجر ابوك .,"http://twitch.tv/Dream%22)
+client.user.setGame(افجر ابوك .,"http://twitch.tv/فجر ابوك)
 client.user.setStatus("dnd")
 });           
 
